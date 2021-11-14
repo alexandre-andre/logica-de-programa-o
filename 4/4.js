5992e9a85d95b6661e37ac09a1ebf256fcc46851
alexandre-andre <andregpf89@gmail.com>
2021-11-14 08:55:45 -0300

    [a-a] insere comentarios nos scripts de 1 a 4

--- 4/4.js
+++ 4/4.js
@@ -3,12 +3,17 @@ function multiples_of_3_or_5(roof) {
     // Write your code here
     let nat = 0
     
+    // i comeca em zero; e enquanto i for < roof; add +1 ao valor de i
     for (let i = 0; i < roof; i++) {
         
+        // se o resto de i/3 = 0
         if ((i % 3) == 0) {
+            // soma i a variavel nat
             nat += i;
             
+            // se o resto de i/5 = 0
         } else if ((i % 5) == 0) {
+            // soma o valor de i a variavel nat
             nat += i;
             
         }  
@@ -16,6 +21,10 @@ function multiples_of_3_or_5(roof) {
     
     return nat
 }
+multiples_of_3_or_5()
+console.log(multiples_of_3_or_5(10))
+
+
 function main() {
     const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
